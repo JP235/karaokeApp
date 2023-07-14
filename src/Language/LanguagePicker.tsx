0875a3ed52933ff7ebc9ba0ee445d3fb5e3c2e62
jpmanti <jpmanti@gmail.com>
2023-07-13 20:52:11 -0500

    nav style

--- src/Language/LanguagePicker.tsx
+++ src/Language/LanguagePicker.tsx
@@ -5,39 +5,54 @@ import { TLanguages } from "../myTypes";
 import { clickAway } from "../components/HelperFunctions";
 import { Language } from "./text";
 
-
 function LanguagePicker() {
-    const [show, setShow] = useState(false)
-    const { language, setLanguage } = useContext(LanguageContext)
+	const [show, setShow] = useState(false);
+	const { language, setLanguage } = useContext(LanguageContext);
 
+	useEffect(() => {
+		document.addEventListener("click", (e) =>
+			clickAway(e, "language-hamb", setShow)
+		);
 
-    useEffect(() => {
-        document.addEventListener("click", (e) => clickAway(e, "language-hamb", setShow))
+		return () => {
+			document.removeEventListener("click", (e) =>
+				clickAway(e, "language-hamb", setShow)
+			);
+		};
+	}, []);
 
-        return () => {
-            document.removeEventListener("click", (e) => clickAway(e, "language-hamb", setShow));
-        };
-    }, []);
-
-    return (
-        <>
-            <div className='language-picker'>
-                <HambButton className="language-hamb" onClick={() => setShow(p => !p)} title={language} />
-                <div className={show ? "language-selector show" : "language-selector"}>
-                    <div className="selector-buttons" n-langs={Object.keys(Language).length}>
-                        {Object.keys(Language).map((key, i) => {
-                            const k = key as TLanguages
-                            return (
-                                <button className={language === k ? "selected-lang" : ""} key={i} onClick={() => setLanguage(k)}>
-                                    {Language[k]}
-                                </button>
-                            )
-                        })
-                        }
-                    </div>
-                </div>
-            </div>
-        </>
-    )
+	return (
+		<>
+			<div className="language-picker">
+				<HambButton
+					className="language-hamb"
+					onClick={() => setShow((p) => !p)}
+					title={language}
+				/>
+				<div className={show ? "language-selector show" : "language-selector"}>
+					<div
+						className="selector-buttons"
+						n-langs={Object.keys(Language).length}
+					>
+						{Object.keys(Language).map((key, i) => {
+							const k = key as TLanguages;
+							return (
+								<span key={i}>
+									<button
+										className={language === k ? "selected-lang" : ""}
+										key={i}
+										onClick={() => setLanguage(k)}
+									>
+										{Language[k]}
+									</button>
+									<span></span>
+								</span>
+							);
+						})}
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
 export default LanguagePicker;
