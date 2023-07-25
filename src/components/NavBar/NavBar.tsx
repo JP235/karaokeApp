883e778e9d9daa78b52b7384edab3e879839e733
jpmanti <jpmanti@gmail.com>
2023-07-25 17:35:14 -0500

    NavBar update

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,10 +1,40 @@
 import LanguagePicker from "../../Language/LanguagePicker";
 import "./NavBar.css";
+import { useEffect, useState } from "react";
 
 export const Navbar = () => {
+	const [isTop, setIsTop] = useState(true);
+
+	const hideNavbar = () => {
+		(document.querySelector(".navbar") as HTMLElement).style.top = "-4rem";
+	};
+
+	const showNavbar = () => {
+		(document.querySelector(".navbar") as HTMLElement).style.top = "0";
+	};
+
+	// useEffect(() => {
+	//   const onScroll = () => {
+	//     let currentPosition = window.pageYOffset;
+	//     if (currentPosition === 0) {
+	//       setIsTop(true);
+	//     } else {
+	//       setIsTop(false);
+	//     }
+	//   };
+	//   document.addEventListener('scroll', onScroll);
+	//   return () => document.removeEventListener('scroll', onScroll);
+	// }, []);
+
+	// useEffect(() => {
+	// 	isTop ? showNavbar() : hideNavbar();
+	// }, [isTop]);
+
 	return (
 		<nav className="navbar">
-			<div className="navbar-title">KaraokeApp</div>
+			<div className="navbar-title">
+				<a href="/">KaraokeApp</a>
+			</div>
 			<LanguagePicker />
 		</nav>
 	);
